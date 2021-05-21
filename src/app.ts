1ad05bd119d7ff61c87de19a2e31bee65ea09ac3
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2021-05-21 21:56:35 +0500

    added cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,18 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import cors from "cors";
+import path from "path";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 export const app = express();
+app.use(cors());
+
+app.use(function (req: Request, res: Response, next: NextFunction) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
