778c33135e5182fd94c1324152ca73149b5fa6db
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2021-05-24 12:40:23 +0500

    some changes

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,19 +1,8 @@
-import {
-  GraphQLList,
-  GraphQLObjectType,
-  GraphQLSchema,
-  GraphQLString,
-} from "graphql";
-import { PostType } from "./TypeDefs/PostType";
+import { GraphQLObjectType, GraphQLSchema } from "graphql";
 import { addUser, userDeleteById } from "./mutatoins/UserMutation";
 import { addPost, deletePostById } from "./mutatoins/PostMutation";
 import { userGetById, userGetByUsername } from "./queries/UserQuery";
 import { postGetById, posts } from "./queries/PostQuery";
-import User from "../User/User";
-import Post from "../Post/Post";
-
-const user = new User();
-const post = new Post();
 
 const RootQuery = new GraphQLObjectType({
   name: "RootQuery",
