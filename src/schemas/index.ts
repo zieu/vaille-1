6f659de13bf957d9a995764404d08e710d4a84fb
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2021-05-27 02:21:06 +0500

    added userFollow mutation

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,5 +1,10 @@
 import { GraphQLObjectType, GraphQLSchema } from "graphql";
-import { addUser, userDeleteById, updateUser } from "./mutations/UserMutation";
+import {
+  addUser,
+  userDeleteById,
+  updateUser,
+  followUser,
+} from "./mutations/UserMutation";
 import { addPost, deletePostById, updatePost } from "./mutations/PostMutation";
 import { userGetById, userGetByUsername } from "./queries/UserQuery";
 import { postGetById, posts } from "./queries/PostQuery";
@@ -23,6 +28,7 @@ const Mutation = new GraphQLObjectType({
     addUser,
     userDeleteById,
     updateUser,
+    followUser,
     // Post Mutations
     addPost,
     deletePostById,
